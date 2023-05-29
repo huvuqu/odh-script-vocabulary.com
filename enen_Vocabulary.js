039a3c280e81c87124cc974191051e42fdce7515
Vũ Quốc Hùng <hungvu.it.96@gmail.com>
2023-05-29 16:39:52 +0700

    Create enen_Vocabulary.js

---
+++ enen_Vocabulary.js
@@ -0,0 +1,13 @@
+Class enen_Vocabulary{
+    constructor() {
+        // Your code starting here ...
+    }
+
+    findTerm(word) {
+        return new Promise((resolve, reject){
+        // Your code starting here ...
+        // resolve(content);
+        // reject(error);
+        });
+    }
+}
